62dab51e673c06db0aa29d65fd9611f305228036
Yazid <yazidbougrine99@gmail.com>
2022-10-29 22:39:55 +0100

    added support for the redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
-import noteReducer, { createNote } from './reducers/noteReducer'
-import filterReducer, { filterChange } from './reducers/filterReducer'
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer,
-})
+import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
 
-const store = createStore(reducer)
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer,
+  },
+})
+console.log(store.getState())
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
